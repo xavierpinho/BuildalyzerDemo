72ab3798865dbaf51329053c22094c8ef4ec256c
Xavier Pinho <xpinho@shiftleft.io>
2022-12-30 12:26:36 +0000

    Initial commit

---
+++ BuildalyzerDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BuildalyzerDemo;
+using Buildalyzer;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var slnFilePath = "/Users/xavierp/Projects/Goatly.NET/GoatlyCore.sln";
+        
+        var manager = new AnalyzerManager(slnFilePath);
+        
+        foreach (var projectAnalyzer in manager.Projects.Values)
+        {
+            Console.Write($"Loading project {projectAnalyzer.ProjectFile.Path} ...");
+            var results = projectAnalyzer.Build();
+            Console.WriteLine("Finished");
+        }
+    }
+}
